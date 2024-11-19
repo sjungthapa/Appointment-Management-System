644a51c605c87e6116168f106e0a2a5edd2e256f
sjungthapa <sjungthapa37@gmail,com>
2024-11-19 19:45:59 +0545

    created usermodel

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    appointments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Appointment'
+    }]
+},{
+    timestamps: true
+})
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
